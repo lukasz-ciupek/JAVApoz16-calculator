70921352f788cbed31eaf63cd369371b80a83624
lukasz-ciupek <lukasz.ciupek@o2.pl>
2019-10-13 10:57:30 +0200

    add new operations

--- src/test/java/pl/sda/spring/CalculatorTest.java
+++ src/test/java/pl/sda/spring/CalculatorTest.java
@@ -27,4 +27,80 @@ public class CalculatorTest {
         //then
         Assert.assertEquals(5.0, actual, 0.00000001);
     }
+
+    @Test
+    public void shouldReturnOneForGivenArgumentsOnSubtractionOperation() {
+        //given
+        double arg1 = 3.0;
+        double arg2 = 2.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.SUBTRACTION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(1.0, actual, 0.00000001);
+    }
+
+    @Test
+    public void shouldReturnSixForGivenArgumentsOnMultiplicationOperation() {
+        //given
+        double arg1 = 3.0;
+        double arg2 = 2.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.MULTIPLICATION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(6.0, actual, 0.00000001);
+    }
+
+    @Test
+    public void shouldReturnOneForGivenArgumentsOnDivisionOperation() {
+        //given
+        double arg1 = 3.0;
+        double arg2 = 3.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.DIVISION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(1.0, actual, 0.00000001);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldReturnRuntimeExceptionForGivenArgumentsOnDivisionOperation() {
+        //given
+        double arg1 = 3.0;
+        double arg2 = 0.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.DIVISION, arg1, arg2);
+
+    }
+
+    @Test
+    public void shouldReturnThreeForGivenArgumentsOnPercentageOperation() {
+        //given
+        double arg1 = 6.0;
+        double arg2 = 50.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.PERCENTAGE, arg1, arg2);
+
+        //then
+        Assert.assertEquals(3.0, actual, 0.00000001);
+    }
+
+    @Test
+    public void shouldReturnSixteenForGivenArgumentsOnPowerOperation() {
+        //given
+        double arg1 = 2.0;
+        double arg2 = 4.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.POWER, arg1, arg2);
+
+        //then
+        Assert.assertEquals(16.0, actual, 0.00000001);
+    }
 }
